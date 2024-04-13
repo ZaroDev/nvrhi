1dac2727f05b1c6977806d219b2356e28fa5bbbc
ZaroDev <victorfalcon796@gmail.com>
2024-04-13 11:10:11 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -6,7 +6,7 @@ cppdialect "C++17"
 targetdir("%{wks.location}/Binaries/" .. outputdir .. "/%{prj.name}")
 objdir("%{wks.location}/Binaries/Intermediates/" .. outputdir .. "/%{prj.name}")
 
-files {"include/**.h", "src/**.cpp", "src/**.h"}
+files {"include/**.h", "src/**.cpp", "src/**.h", "thirdparty/**.h", "thirdparty/**.hpp"}
 includedirs {"include", "src", "thirdparty/Vulkan-Headers/include"}
 
 filter "system:windows"
